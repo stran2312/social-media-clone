062509b9f6cf26cc00a86b2f8bd309b57874883d
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-06-02 00:11:46 -0500

    Add files via upload

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,16 +1,18 @@
-$(document).ready(function () {
-	// on click sigh up, hide login and show registration form
-	$("#signup").click(function () {
-		$("#first").slideUp("slow", function() {
+$(document).ready(function() {
+
+	//On click signup, hide login and show registration form
+	$("#signup").click(function() {
+		$("#first").slideUp("slow", function(){
 			$("#second").slideDown("slow");
-		})
+		});
 	});
 
-	// on click sigh in, hide registration and show login form
-	$("#signin").click(function () {
-		$("#second").slideUp("slow", function() {
+	//On click signup, hide registration and show login form
+	$("#signin").click(function() {
+		$("#second").slideUp("slow", function(){
 			$("#first").slideDown("slow");
-		})
+		});
 	});
+
 
 });
